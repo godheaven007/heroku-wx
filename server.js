5b665449af7d78b7016c61bba2f29677f70c90ff
godheaven007 <345777292@qq.com>
2019-08-12 17:08:39 +0800

    readme add

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+const app = express()
+const sha1 = require('sha1')
+
+
+app.get('/', (req, res) => {
+    res.send('test');
+});
+
+
+let port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log(`listening on port ${port}!`)
+})
+
